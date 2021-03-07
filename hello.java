2da207d3e57635875b5a2340b0b80d2b9ab9e7e6
Devops5-engi <engineerdevops8@gmail.com>
2021-03-07 17:55:16 +0530

    hello file chaged

--- hello.java
+++ hello.java
@@ -1,10 +1,18 @@
-class Test{
+cilass Test{
 	public static void main(String[] args){
 	
 		System.out.println("Welcome to devops git");
 		System.out.println("devops git");
 		System.out.println("sfdfdsfdsfdsfdf");
-		 System.out.println("sfdfd");
+		
+		System.out.println("sfdfd");
+
+	
+
+		 for(int i=1; i<=10; i++){
+			 System.out.println(i);
+		 }
+		
 
 	}
 }
